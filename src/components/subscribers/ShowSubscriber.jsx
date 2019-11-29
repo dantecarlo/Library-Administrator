d7be09f1c849229b1b6078ba09d55a1a542916a5
dantecarlo <dante.cnp@gmail.com>
2019-11-29 16:19:32 -0500

    ADD Show Subscriber functionality

--- src/components/subscribers/ShowSubscriber.jsx
+++ src/components/subscribers/ShowSubscriber.jsx
@@ -1,11 +1,67 @@
 import React from 'react'
+import { compose } from 'redux'
+import { connect } from 'react-redux'
+import { firestoreConnect } from 'react-redux-firebase'
+import { Link } from 'react-router-dom'
+import { PropTypes } from 'prop-types'
 
-const ShowSubscriber = () => {
+import Spinner from '../layout/spinner'
+
+const ShowSubscriber = ({ subscriber }) => {
+  console.log(subscriber)
+  if (!subscriber) return <Spinner />
+
   return (
     <div>
-      <h1>Show Subscriber</h1>
+      <div className="row">
+        <div className="col-md-6 mb-4">
+          <Link to="/subscribers" className="btn btn-secondary">
+            <i className="fas fa-arrow-circle-left"> Back to Subscribers</i>
+          </Link>
+        </div>
+        <div className="col-md-6">
+          <Link
+            to={`/subscribers/edit/${subscriber.id}`}
+            className="btn btn-primary float-right"
+          >
+            <i className="fas fa-pencil-alt"> Edit Subscriber</i>
+          </Link>
+        </div>
+        <hr className="mx-5 w-100" />
+
+        <div className="col-12">
+          <h2 className="mb-4">
+            {subscriber.name} {subscriber.lastName}
+          </h2>
+
+          <p>
+            <span className="font-weight-bold">Career: </span>
+            {subscriber.career}
+          </p>
+
+          <p>
+            <span className="font-weight-bold">Code: </span>
+            {subscriber.code}
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
 
-export default ShowSubscriber
+ShowSubscriber.propTypes = {
+  firestore: PropTypes.object.isRequired
+}
+
+export default compose(
+  firestoreConnect(props => [
+    {
+      collection: 'susbcribers',
+      storeAs: 'subscriber',
+      doc: props.match.params.id
+    }
+  ]),
+  connect(({ firestore: { ordered } }, props) => ({
+    subscriber: ordered.subscribers && ordered.subscribers[0]
+  }))
+)(ShowSubscriber)
